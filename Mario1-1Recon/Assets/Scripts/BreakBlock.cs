7954bdb42405da50333c786a8adbf745eec6dda6
Joshua <konohashi@gmail.com>
2016-02-12 02:26:54 -0500

    start of being super mario started

--- Mario1-1Recon/Assets/Scripts/BreakBlock.cs
+++ Mario1-1Recon/Assets/Scripts/BreakBlock.cs
@@ -5,12 +5,22 @@ public class BreakBlock : MonoBehaviour {
 
 	//public float opposingForce = -950f;
 
+	Animator anim;
+
+	int brick = Animator.StringToHash ("Break");
+
+
+	void Start (){
+		anim = GetComponent<Animator> ();
+
+	}
 
 	private void OnTriggerEnter2D(Collider2D other)
 	{
-		if (other.name == "HeadCheck" )
+		if (other.name == "HeadCheck" && GameObject.Find("Mario").GetComponent<Player_controller>().isSuper == true )
 		{
 			Debug.Log ("PLAYER HIT");
+			anim.SetBool (brick, true);
 			Destroy(this.gameObject);
 			//other.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, opposingForce));
 		}
